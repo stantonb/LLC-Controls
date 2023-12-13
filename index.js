af93350e0fe3242af7dcd83b885cc4660dca6b72
Stanton Borthwick <stanton.borthwick@gmail.com>
2023-12-13 22:37:41 +0000

    hardcoded my firms, and got the loops working, now roughly correctly works out profits

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ async function main(){
 	let walletInfo = await getWalletInfo();
 	
 	let firms = await getProfitableFirms(walletInfo);
-	console.log(firms);
+	// console.log(firms);
 }
 
 async function getProfitableFirms(walletInfo){
@@ -62,14 +62,51 @@ async function getMarketInfo(){
 }
 
 async function getProfit(firm, marketInfo){
+	console.log(firm);
+
 	let firmType = firm.type;
 	let firmInputs = firmData[firmType]?.inputs;
 	let firmOutputs = firmData[firmType]?.outputs;
+	let firmProfit = 0;
 
-	console.log(firmInputs);
-	console.log(firmOutputs);
+	if (!firmInputs || !firmOutputs) {
+		console.log('No inputs or outputs found for ' + firmType);
+		return 0;
+	}
+
+	//loop through inputs and get market price
+	for(let key in firmInputs){
+		let input = firmInputs[key];
+		let marketInputPrice = marketInfo?.resp[key][0]/100;
+
+		if (!input || !marketInputPrice) {
+			console.log('No market input found for ' + input?.type);
+			return 0;
+		}
+
+		firmProfit += input * marketInputPrice;
+	}
+
+	//loop through outputs and get market price
+	for(let key in firmOutputs){
+		let output = firmOutputs[key];
+
+		// console.log('Key: ' + key);
+		// console.log(marketInfo?.resp[key]);
+
+		let marketOutputPrice = marketInfo?.resp[key][0]/100;
+
+		if (!output || !marketOutputPrice) {
+			console.log('No market output found for ' + output?.type);
+			return 0;
+		}
+
+		firmProfit += output * marketOutputPrice;
+	}
 	
-	return 1;
+	console.log(firmType + ' profit: ' + firmProfit);
+
+	return firmProfit;
 }
 
 main();
